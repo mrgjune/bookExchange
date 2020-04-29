9da5e4e3df86c4b9a45b618b9e43833ca3f7d9a4
Mara Greene <mrgjune@gmail.com>
2020-04-29 16:19:59 -0700

    file name chagne

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,6 +1,6 @@
 const db = require("../db");
-const ExpressError = require("helpers/ExpressError");
-const partialUpdate = require("helpers/partialUpdate");
+const ExpressError = require("../helpers/expressError");
+const partialUpdate = require("../helpers/partialUpdate");
 const bcrypt = require("bcrypt");
 const BCRYPT_WORK_FACTOR = 10;
 
